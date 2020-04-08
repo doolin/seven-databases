6b23109f843f8b94a079beb6b478dec6656b71b1
dave doolin <david.doolin@gmail.com>
2020-04-08 06:48:16 -0700

    ch8 - set up for working with Redis

--- ch8/readme.md
+++ ch8/readme.md
@@ -1,1 +1,6 @@
 # Redis
+
+Currently running Redis via Docker: `docker run -p 6379:6379 -d redis`,
+version 5.0.5.
+
+
---
+++ ch8/redis-test.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+require 'redis'
+
+$redis = Redis.new(host: '127.0.0.1', port: 6379)
+$redis.flushall
+$redis.set 'foo', 'bar'

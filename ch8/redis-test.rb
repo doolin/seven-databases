d88cda748a39dbfe60f168338dffff75c55ad353
dave doolin <david.doolin@gmail.com>
2020-04-09 04:44:10 -0700

    p. 263, ch8 - set/get and mset/mget

--- ch8/redis-test.rb
+++ ch8/redis-test.rb
@@ -1,7 +1,28 @@
 #!/usr/bin/env ruby
 
 require 'redis'
+require 'rspec/autorun'
 
 $redis = Redis.new(host: '127.0.0.1', port: 6379)
 $redis.flushall
-$redis.set 'foo', 'bar'
+# $redis.set 'foo', 'bar'
+
+RSpec.describe self do
+  example 'set a key' do
+    key = 'foo'
+    value = 'bar'
+    $redis.set key, value
+    expected = $redis.get(key)
+    expect(expected).to eq value
+  end
+
+  example 'mset / mget' do
+    k1 = 'gog'
+    v1 = 'google'
+    k2 = 'yah'
+    v2 = 'yahoo'
+    $redis.mset(k1, v1, k2, v2)
+    expected = $redis.mget(k1, k2)
+    expect(expected).to eq [v1, v2]
+  end
+end
